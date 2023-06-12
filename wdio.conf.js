b0f3ab610408fbf9f80df1594e3507d80580b31f
Grigoryan <voskan.grigoryan@accenture.com>
2023-06-12 12:23:52 -0300

    adding reportportal, not working either

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,32 @@
+const reportportal = require("wdio-reportportal-reporter");
+const RpService = require("wdio-reportportal-service");
+
+const conf = {
+    reportPortalClientConfig: {
+        // report portal settings
+        token: "00000000-0000-0000-0000-00000000000",
+        endpoint: "https://reportportal-url/api/v1",
+        launch: "launch_name",
+        project: "project_name",
+        mode: "DEFAULT",
+        debug: false,
+        description: "Launch description text",
+        attributes: [{ key: "tag", value: "foo" }],
+        headers: { foo: "bar" }, // optional headers for internal http client
+    },
+    reportSeleniumCommands: false, // add selenium commands to log
+    seleniumCommandsLogLevel: "debug", // log level for selenium commands
+    autoAttachScreenshots: false, // automatically add screenshots
+    screenshotsLogLevel: "info", // log level for screenshots
+    parseTagsFromTestTitle: false, // parse strings like `@foo` from titles and add to Report Portal
+    cucumberNestedSteps: false, // report cucumber steps as Report Portal steps
+    autoAttachCucumberFeatureToScenario: false, // requires cucumberNestedSteps to be true for use
+    isSauseLabRun: false, // automatically add SauseLab ID to rp tags.
+};
+
 exports.config = {
+    reporters: [[reportportal, conf]],
+
     wdi5: {
         screenshotPath: require("path").join("zerotouch_billingrequestor", "webapp", "test", "screenshots"),
         screenshotsDisabled: false,
@@ -40,23 +68,11 @@ exports.config = {
 
     connectionRetryCount: 3,
 
-    services: ["chromedriver", "firefox-profile", "ui5"],
+    services: [[RpService, {}]],
 
     framework: "mocha",
 
     // reporters: ["spec"],
-
-    reporters: [
-        "spec",
-        [
-            "allure",
-            {
-                outputDir: "allure-results",
-                disableWebdriverStepsReporting: false,
-                disableWebdriverScreenshotsReporting: false,
-            },
-        ],
-    ],
 
     //
     // Options to be passed to Mocha.
